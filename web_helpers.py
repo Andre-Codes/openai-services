359b2cd5a5a54dcbbd7aabdaa9a9ef2f1fa46a75
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-09-29 03:54:49 -0500

    refactored structure

--- web_helpers.py
+++ web_helpers.py
@@ -6,10 +6,12 @@ def generate_response(app, prompt):
     with st.spinner('...thinking :thought_balloon:'):
         return app.get_response(prompt=prompt, format_style='markdown')
 
+
 def handle_file_output(responses, all_response_content):
     all_response_content.append(f"{responses} \n\n")
     file_data = ''.join(all_response_content)
     return file_data
+
 
 def create_download(response, role_name):
     st.download_button(
@@ -17,7 +19,8 @@ def create_download(response, role_name):
         data=response,
         file_name=f'{role_name}.md',
         mime='text/markdown'
-    )  
+    )
+
 
 def display_response(response, assistant, all_response_content, role_name, streaming):
     st.divider()
@@ -32,14 +35,14 @@ def display_response(response, assistant, all_response_content, role_name, strea
                     collected_responses.append(content_chunk)
                     response_content = ''.join(collected_responses)
                     markdown_placeholder.markdown(f"{response_content}\n\n")
-        
-        file_data = handle_file_output(response_content, all_response_content) # not working with extra lesson
+
+        file_data = handle_file_output(response_content, all_response_content)  # not working with extra lesson
     else:
         response_content = response['choices'][0]['message']['content']
         markdown_placeholder.markdown(response_content)
         file_data = response_content
-    
+
     if assistant:
         create_download(file_data, role_name)
-        
+
     return response_content
