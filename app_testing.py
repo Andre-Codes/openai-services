7452d0d87956321aad10d75c2623aa5869a35279
Andre <amardian22@gmail.com>
2024-11-08 17:27:10 -0600

    added more capabilities

--- app_testing.py
+++ app_testing.py
@@ -21,7 +21,16 @@ def stream(chunk, container: list):
 
 
 text = st.text_area("Prompt")
-file = st.file_uploader("Upload Image")
+file = st.file_uploader("Upload Image", accept_multiple_files=True)
+
+files = [
+    r"C:\Users\havok\OneDrive\Pictures\AI Art\aragorn_arwen_impres_fantasy.png",
+    r"C:\Users\havok\OneDrive\Pictures\AI Art\artsy_fibonacci.png"
+]
+urls = [
+    "https://images.ctfassets.net/cnu0m8re1exe/4luBczkxnxMMVO4gTiz0vD/6f290be6f5340264ddc1fe0977134a90/Untitled_design_-_2023-06-20T145027.117.png?fm=jpg&fl=progressive&w=660&h=433&fit=fill",
+    "https://i.natgeofe.com/n/5d00b0cc-ab95-4522-ad13-7c65b7589e6b/NationalGeographic_748483.jpg?w=636&h=424"
+]
 
 text_prompt = None
 if st.button("Go"):
@@ -46,8 +55,12 @@ if st.button("Go"):
         streamer.run()
     else:
         # revised_prompt defaults to True
-        st.image(reply[0][0])
-        st.write(reply[0][1])
+        if isinstance(reply, list):
+            for img in reply:
+                st.image(img[0])
+        else:
+            st.image(reply[0][0])
+            st.write(reply[0][1])
 
 st.divider()
 
